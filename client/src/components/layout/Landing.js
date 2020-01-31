49e8f5cdc184db07299bfc12b097da9c6e75c05d
unknown <Chad>
2020-01-30 21:42:50 -0800

    logout

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Landing = () => {
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Inventory-Management</h1>
+          <p className='lead'>
+            Easy and efficient way to track your Resell Inventory!
+          </p>
+          <div className='buttons'>
+            <Link to='/register' className='btn btn-primary'>
+              Sign Up
+            </Link>
+            <Link to='/login' className='btn btn'>
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
